374a9e37b4d97e8f374883c87d0b40c1d1d7c668
JeanWolff10 <41656019+JeanWolff10@users.noreply.github.com>
2018-07-24 20:07:24 -0600

    Delete parametres.js

--- parametres.js
+++
@@ -1,9 +0,0 @@
-var frictionBalles=0.3;
-var restitutionBalles=0.6;
-var rayonBalles=17;
-
-var angle_mort=1; // en radians
-var persistence_mort=2;
-var hauteur_saut=0.02;
-var vitesse_laterale=0.001;
-var couleurs=[[255,0,0],[0,0,255],[255,255,0],[0,255,255],[255,0,255],255];
