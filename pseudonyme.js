965c836514be875578f292b2d70a6ae23e4a38f3
JeanWolff10 <41656019+JeanWolff10@users.noreply.github.com>
2018-07-24 20:07:30 -0600

    Delete pseudonyme.js

--- pseudonyme.js
+++
@@ -1,9 +0,0 @@
-function demander_pseudonyme(){
-  var usertest=0;
-  if (usertest==0) {
-    monPseudonyme=prompt("Pseudonyme")
-    usertest=1;
-    console.log(monPseudonyme);
-    socket.emit('pseudonyme_aller',monPseudonyme);
-  }
-}
