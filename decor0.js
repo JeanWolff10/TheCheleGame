d043d16e66e87ac94d82e2636e73c047ead48f1a
JeanWolff10 <41656019+JeanWolff10@users.noreply.github.com>
2018-07-24 20:05:43 -0600

    Delete decor0.js

--- decor0.js
+++
@@ -1,7 +0,0 @@
-function decor0() {
-  // 800, 400
-  boundaries.push(new boundary(width/4-15, height-10, width/2-30, 20, 0));
-  boundaries.push(new boundary(3*width/4+15, height-10, width/2-30, 20, 0));
-  boundaries.push(new boundary(width/2, 150, 150, 20, 0));
-  boundaries.push(new boundary(width/2+100, 230, 100, 20, -0.5));
-}
